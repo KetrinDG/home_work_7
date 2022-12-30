f9905a380c61541d7ca6b1810464160e2122d996
KetrinDG <degtyareva.ev1@gmail.com>
2022-12-30 14:20:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name='clean',
+setup(
+      name='clean',
       version='1.0',
       description='A flexible module to clean folder',
       author='author',
       author_email='degtyareva.ev1@gmail.com',
       url='',
       modules=['clean'],
+      entry_points={'console_scripts': ['pip install -e']},
       packages = find_packages(where="clean_folder"),
       package_dir = {"": "clean_folder"},
-      include_package_data=True)
+      include_package_data=True
+      )
